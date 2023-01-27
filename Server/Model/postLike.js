3ef824fa42c38c056ef607d9369292e217106012
Aditya Gupta <67819043+aadityazz@users.noreply.github.com>
2023-01-27 11:19:58 +0530

    First

--- Server/Model/postLike.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const postSchema = mongoose.Schema({
-    likes: { type: [String], default: [] }
-})
-
-var PostLike = mongoose.model('PostLike', postSchema);
-
-export default PostLike;
